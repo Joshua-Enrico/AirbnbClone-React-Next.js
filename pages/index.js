5994a39d91d9dd561ecdbc0d93e6f7089738fe9e
yoyogold-a11 <tmrfack@gmail.com>
2021-11-23 23:57:34 -0500

    Header Component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
+import Header from '../Components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Stating</p>
+      { /* Header */ }
+      <Header />
+      { /* Banner */ }
     </div>
   )
 }
